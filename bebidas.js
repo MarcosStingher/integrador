99d5177ef425461e286ab032b470d724a694c6bb
Marcos Stingher <111930382+MarcosStingher@users.noreply.github.com>
2022-11-13 13:02:35 -0300

    Update bebidas.js

--- bebidas.js
+++ bebidas.js
@@ -1,9 +1,11 @@
 let pizzaJson = [
-    {id:1, name:'SMIRNOFF Red Original', img:'images/bebida1.png', price:8.90, sizes:['275ml', '1L'], description:'Bebida Alcoólica Mista de Limão SMIRNOFF Red Original Garrafa 275ml'},
+    {id:1, name:'SMIRNOFF Red Original', img:'images/bebida1.png', price:8.90, sizes:['275ml', '1L'], description:'Bebida Alcoólica Mista de Limão SMIRNOFF Red Original Garrafa'},
     {id:2, name:'Vinho Chileno Bicicleta Cabernet', img:'images/bebida2.png', price:56.50, sizes:['275ml','860ml'], description:'Harmoniza com carnes grelhadas, de forno, queijos e embutidos'},
     {id:3, name:'Vodka SMIRNOFF Infusion Fruit', img:'images/bebida3.png', price:54.90, sizes:['275ml','860ml'], description:'Tem um sabor robusto com acabamento seco para uma maior suavidade'},
     {id:4, name:'Vodka ABSOLUT Apeach', img:'images/bebida4.png', price:99.90, sizes:['275ml', '860ml'], description:'Seu sabor é suave e delicado, com uma presença sofisticada do pêssego.'},
     {id:5, name:'Vodka Ciroc Original', img:'images/bebida5.png', price:112.90, sizes:['275ml', '860ml'], description:'Destilado alcoólico simples de uva retificado e água'},
     {id:6, name:'Gin Tanqueray London', img:'images/bebida6.png', price:128.90, sizes:['275ml', '860ml'], description:'Alcoól etílico de cereais retificado, água, extratos naturais de bagas de zimbro e outras substâncias aromáticas'},
-    {id:7, name:'JACK DANIELS', img:'images/bebida7.png', price:159.99, sizes:['275ml', '860ml'], description:'Whisky Americano Tennessee Fire JACK DANIELS 1 Litro'}
+    {id:7, name:'Jack Daniels', img:'images/bebida7.png', price:159.90, sizes:['275ml', '860ml'], description:'Whisky Americano Tennessee Fire Jack Daniels'},
+    {id:8, name:'Red Label', img:'images/bebida8.jpg', price:139.90, sizes:['275ml', '860ml'], description:'Whisky Escocês Johnnie Walker Red Label'},
+    {id:9, name:'Passport Scotch', img:'images/bebida9.png', price:79.90, sizes:['275ml', '860ml'], description:'Whisky Escocês Passport Scotch'}
 ];
